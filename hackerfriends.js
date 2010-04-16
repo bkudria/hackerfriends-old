6c1f628f7058133e6cd320721a54eeed0a180183
Benjamin Kudria <ben@kudria.net>
2010-04-16 02:19:00 -0700

    niceties including screenshots and icons and version numbers

--- hackerfriends.js
+++ hackerfriends.js
@@ -4,8 +4,8 @@ function render() {
     $("a[href^='user?id']").each(function(index, link) {
         var username = $(link).text();
         var addremove = $('<span class="add_remove"></span>');
-        var add = $('<a href="#" class="add">&nbsp;+&nbsp;</a>').click(function(e) {chrome.extension.sendRequest({add_user: username}, function(response) {render();})});
-        var remove = $('<a href="#" class="remove">&nbsp;-&nbsp;</a>').hide().click(function(e) {chrome.extension.sendRequest({remove_user: username}, function(response) {render();})});
+        var add = $('<a href="#" class="add">&nbsp;+&nbsp;</a>').click(function(e) {e.preventDefault(); chrome.extension.sendRequest({add_user: username}, function(response) {render();})});
+        var remove = $('<a href="#" class="remove">&nbsp;-&nbsp;</a>').hide().click(function(e) {e.preventDefault(); chrome.extension.sendRequest({remove_user: username}, function(response) {render();})});
         $(link).after(addremove.append(add).append(remove));
     });
     chrome.extension.sendRequest({get_users: true}, function(response) {
